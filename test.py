f299717e2c5572c378fa0ce70528d9f5b7144a79
Sujatro Ganguli <iamsurjog@gmail.com>
2025-07-03 23:29:22 +0530

    feat: added an example for the rag so its easier to use

---
+++ test.py
@@ -0,0 +1,15 @@
+# Example code of how to use the RAG
+
+from rag import Core
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL")
+LANGUAGE_MODEL = os.getenv("LANGUAGE_MODEL")
+
+rag = Core(EMBEDDING_MODEL, LANGUAGE_MODEL)
+rag.add_files("telephone.txt")
+print(rag.generate("how to fix telephone in 500 words or less"))
+print("-" * 80)
+print(rag.generate("now summarize it in 100 words or less"))
