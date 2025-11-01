b1e4b9a7c4e5b26e9dcb65c722a3471b7298b447
iamsurjog <iamsurjog@gmail.com>
2025-11-01 21:21:12 +0530

    feat: added dummy values and created empty kuzu file

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Query(BaseModel):
+    query: str
+
+class Context(BaseModel):
+    # dummy value
+    something: str
+
+@app.get("/query")
+async def query(query:Query):
+    pass
+
+@app.get("/answer")
+async def answer():
+    pass
+
+@app.get("/expand")
+async def expand(context:Context):
+    pass
+
+@app.get("/graph")
+async def graph():
+    pass
