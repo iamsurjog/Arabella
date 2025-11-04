cfef1569db243bcde3178461645b7880dede0feb
iamsurjog <iamsurjog@gmail.com>
2025-11-04 08:58:57 +0530

    feat: removed /expand cuz automate it

--- main.py
+++ main.py
@@ -6,20 +6,12 @@ app = FastAPI()
 class Query(BaseModel):
     query: str
 
-class Context(BaseModel):
-    # dummy value
-    something: str
-
 @app.get("/query")
 async def query(query:Query):
     pass
 
 @app.get("/answer")
 async def answer():
-    pass
-
-@app.get("/expand")
-async def expand(context:Context):
     pass
 
 @app.get("/graph")
