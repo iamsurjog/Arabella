a40d98d4ded9413a1d38dcaf3a2649e7107f1660
iamsurjog <iamsurjog@gmail.com>
2025-11-06 11:13:23 +0530

    feat: end point added session_id and made them post

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+from types import new_class
 from fastapi import FastAPI
+from fastapi.routing import request_response
 from pydantic import BaseModel
 
 app = FastAPI()
 
+class Graph(BaseModel):
+    session_id: str
+
 class Query(BaseModel):
+    session_id: str
     query: str
+    new_session: bool | None = True
 
-@app.get("/query")
+@app.post("/query")
 async def query(query:Query):
-    pass
+    return f"{query.session_id  = }, {query.query  = }, {query.new_session  = }"
 
+## TODO: Do we even need this?
 @app.get("/answer")
 async def answer():
     pass
 
-@app.get("/graph")
-async def graph():
-    pass
-
+@app.post("/graph")
+async def graph(graph:Graph):
+    return f"{graph.session_id = }"
